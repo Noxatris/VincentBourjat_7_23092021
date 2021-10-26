f2495fe7d9a933bcad5976bc7e4ca87dec181f66
Vincent Bourjat <vincentbourjat@gmail.com>
2021-10-26 23:29:45 +0200

    Mise en place des styles des messages

--- frontend/src/components/ListeMessage.vue
+++ frontend/src/components/ListeMessage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="conteneur_message">
+    <div class="liste_message">
     
     </div>
 </template>
@@ -45,11 +45,13 @@ export default ({
             tabMessage[0] = document.createElement('div'); // Div Principale
             tabMessage[1] = document.createElement('div'); // Div Nom / supprimer le message
             tabMessage[2] = document.createElement('div'); // div Texte
-            tabMessage[3] = document.createElement('div'); // div image
             tabMessage[4] = document.createElement('div'); // div like 
             tabMessage[5] = document.createElement('div'); // Div commentaire
 
             tabMessage[6] = document.createElement('p'); // Conteneur nom
+
+
+            tabMessage[1].appendChild(tabMessage[6]);
 
             if(donnee["author"] == this.userId || this.role == "admin"){
                 tabMessage[7] = document.createElement('input'); // button
@@ -61,31 +63,57 @@ export default ({
 
             tabMessage[8] = document.createElement('p'); // Texte du message
 
-            tabMessage[9] = document.createElement('img'); // Image du message
+            if(donnee["imageUrl" != null]){
+                tabMessage[3] = document.createElement('div'); // div image
+                tabMessage[0].appendChild(tabMessage[3]);
+                tabMessage[9] = document.createElement('img'); // Image du message
+                tabMessage[3].appendChild(tabMessage[9]);
 
-            tabMessage[10] = document.createElement('input'); // Bouton Like
-            tabMessage[11] = document.createElement('input'); // Bouton dislike
+                // Ajout de l'image
+                tabMessage[9].setAttribute("src", donnee["imageUrl"]);
+            }
+
+            tabMessage[10] = document.createElement('a'); // Bouton Like
+            tabMessage[11] = document.createElement('a'); // Bouton dislike
 
             tabMessage[12] = document.createElement('input'); // Button commentaire
 
+            tabMessage[13] = document.createElement('div'); // Conteneur commentaire
+
+            tabMessage[14] = document.createElement('p'); // Nombre Like
+            tabMessage[15] = document.createElement('p'); // Nombre Dislike
+
+            tabMessage[16] = document.createElement('i'); // Icone Like
+            tabMessage[17] = document.createElement('i'); // Icone Dislike
+
+            tabMessage[18] = document.createElement('i'); // Icone like remplie
+            tabMessage[19] = document.createElement('i'); // Icone Dislike remplie
+
             tabMessage[0].appendChild(tabMessage[1]);
             tabMessage[0].appendChild(tabMessage[2]);
-            tabMessage[0].appendChild(tabMessage[3]);
             tabMessage[0].appendChild(tabMessage[4]);
             tabMessage[0].appendChild(tabMessage[5]);
-
-            tabMessage[1].appendChild(tabMessage[6]);
             
-
             tabMessage[2].appendChild(tabMessage[8]);
 
-            tabMessage[3].appendChild(tabMessage[9]);
-
+            // Like / Dislike
             tabMessage[4].appendChild(tabMessage[10]);
             tabMessage[4].appendChild(tabMessage[11]);
+            
+
+            tabMessage[10].appendChild(tabMessage[16]);
+            tabMessage[10].appendChild(tabMessage[17]);
+            tabMessage[10].appendChild(tabMessage[14]);
+
+            tabMessage[11].appendChild(tabMessage[18]);
+            tabMessage[11].appendChild(tabMessage[19]);
+            tabMessage[11].appendChild(tabMessage[15]);
 
             tabMessage[5].appendChild(tabMessage[12]);
 
+
+
+            // Récupération et affichage du nom de l'auteur
             fetch('http://localhost:3000/api/auth/' + donnee["author"], { 
                 method: 'GET',
                 headers:{
@@ -104,19 +132,41 @@ export default ({
             })
             .catch()
             
+            // Ajout du texte du message
             tabMessage[8].innerText = donnee["contenu"];
-            tabMessage[9].setAttribute("src", donnee["imageUrl"]);
 
+            // Gestion Like / Dislike
+            tabMessage[14].innerText = donnee["like"];
+            tabMessage[15].innerText = donnee["dislike"]
+
+            // Gestion Commentaire
+            tabMessage[12].setAttribute("type", "button");
+            tabMessage[12].setAttribute("value", "Commentaires");
+            
             tabMessage[0].classList.add("conteneur_message");
             tabMessage[1].classList.add("conteneur_texte");
-            tabMessage[0].classList.add("conteneur");
-            tabMessage[0].classList.add();
-            tabMessage[0].classList.add();
-            tabMessage[0].classList.add();
+            tabMessage[2].classList.add("conteneur_contenu");
+            tabMessage[4].classList.add("conteneur_avis");
+            tabMessage[5].classList.add("conteneur_btn_commentaire");
+
+
+            tabMessage[16].classList.add("far");
+            tabMessage[16].classList.add("fa-thumbs-up");
+
+            tabMessage[17].classList.add("far");
+            tabMessage[17].classList.add("fa-thumbs-down");
+
+            tabMessage[18].classList.add("fas");
+            tabMessage[18].classList.add("fa-thumbs-up");
+            tabMessage[18].classList.add("invisible");
+
+            tabMessage[19].classList.add("fas");
+            tabMessage[19].classList.add("fa-thumbs-down");
+            tabMessage[19].classList.add("invisible");
             
 
             
-            let conteneurMessage = document.getElementsByClassName("conteneur_message");
+            let conteneurMessage = document.getElementsByClassName("liste_message");
             conteneurMessage[0].appendChild(tabMessage[0]);
         },
     },
@@ -126,6 +176,87 @@ export default ({
 })
 </script>
 
-<style scoped>
+<style>
+    .conteneur_message{
+        display: flex;
+        flex-direction: column;
+        width: 95%;
+        margin: 20px auto;
+        border: 1px solid black;
+        border-radius: 15px;
+    }
+    
+    /* Titre + bouton suppr */
+    .conteneur_texte{
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid black;
+    }
+
+    .conteneur_texte p{
+        width: 20%;
+        margin-left: 10px;
+    }
+
+    .conteneur_texte input{
+        border-radius: 15px;
+        height: 30px;
+        width: 30px;
+        margin: auto 10px;
+    }
+
+    /* Avis */
+    .conteneur_avis {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        position: relative;
+        height: 35px;
+    }
+
+    .conteneur_avis p {
+        margin: 0 15px;
+    }
+
+    .conteneur_avis svg{
+        position: absolute;
+        cursor: pointer;
+        height: 50%;
+    }
+
+    .conteneur_avis .fa-thumbs-up{
+        right: 19%;
+    }
+
+        .conteneur_avis .fa-thumbs-down{
+        right: 8%;
+    }
+
+    .invisible{
+        opacity: 0;
+    }
+
+    /* Texte */
+    .conteneur_contenu{
+        display: flex;
+        justify-content: flex-start;
+        border-bottom: 1px solid black;
+    }
+
+    .conteneur_contenu p {
+        margin-left: 5px;
+    }
+
+    /* Commentaire (bouton) */
+    .conteneur_btn_commentaire{
+        width: 100%;
+        overflow: hidden;
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+    }
+    .conteneur_btn_commentaire input{
+        width: 100%;
+        height: 25px;
+    }
 
 </style>
